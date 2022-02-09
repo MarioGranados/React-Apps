9132a1f77f8fb6bc11dbc2dc27c42f45d1374f8c
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-09 11:51:55 -0600

    new

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import Aos from "aos";
 import 'aos/dist/aos.css';
 import Curtains from "./components/Curtains/Curtains";
 
+
 function App() {
     Aos.init();
     return (
--- portfolio/src/components/Curtains/Curtains.js
+++ portfolio/src/components/Curtains/Curtains.js
@@ -6,9 +6,9 @@ function Curtains() {
             <div className='vh-100 bg-dark align-items-center d-flex justify-content-center'>
                 <div className="text-center green-text display-6">Scroll Down</div>
             </div>
-            <div className='curtain_right'>
+            <div className='curtain_right bg-dark' data-aos='fade-in' data-aos-duration='1' data-aos-once={'false'}>
             </div>
-            <div className='curtain_left'>
+            <div className='curtain_left bg-dark' data-aos='fade-in' data-aos-duration='1' data-aos-once={'false'}>
             </div>
         </>
     )
